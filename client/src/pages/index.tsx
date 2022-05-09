fa7621ed4f6ca298967243f63330d1004efad9c1
intla <59796115+ovoleur@users.noreply.github.com>
2022-05-09 15:21:49 +0200

    websocket test

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,3 +1,12 @@
-const Index = () => <div>Hello</div>;
+import { io } from "socket.io-client";
+
+const Index = () => {
+  const socket = io("ws://localhost:4000");
+  //   socket.connect();
+  socket.on("hello", (arg) => {
+    console.log(arg); // world
+  });
+  return <div>Hello</div>;
+};
 
 export default Index;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,22 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
-const port = 3000;
+const port = 4000;
+
+const server = createServer(app);
+const io = new Server(server);
 
 app.get("/", (_, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+io.on("connection", (socket) => {
+  socket.emit("hello", "world");
+  console.log("Connection !");
+});
+
+server.listen(port, () => {
+  console.log(`Server listening to port ${port}`);
 });
