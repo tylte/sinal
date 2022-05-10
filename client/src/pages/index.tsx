0fe426a7dfe57196bf4b5a0c2c9f9ad7f9697d15
BenjViaud <benjamin.viaud@etudiant.univ-rennes1.fr>
2022-05-10 11:39:57 +0200

    json and send

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,14 +1,14 @@
-// import { io } from "socket.io-client";
+import { io } from "socket.io-client";
 
 import { GameModeMenu } from "../components/GameModeMenu";
 import { Layout } from "../components/Layout";
 
 const Index = () => {
-  //   const socket = io("ws://localhost:4000");
-  //   socket.connect();
-  //   socket.on("hello", (arg) => {
-  //     console.log(arg); // world
-  //   });
+  const socket = io("ws://localhost:4000");
+  socket.connect();
+  socket.on("hello", (arg) => {
+       console.log(arg); // world
+ });
   return (
     <Layout>
       <GameModeMenu />
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,31 +2,31 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { get_word, get_id } from"./Endpoint/start_game";
-export interface IHash {
-  [details: string] : string;
-} 
 
-var tab : IHash = {};
-let id;
-let word;
+var tab : Map<string,string> = new Map();
 
 const app = express();
 const port = 4000;
 
 const server = createServer(app);
 const io = new Server(server);
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get("/", (_, res) => {
   res.send("Hello World!");
 });
 
+app.post("/start_game", (req, res) => {
+ let id = get_id();
+ let word = get_word();
+ tab.set(id, word);
+ console.log(req.body.mode);
+ res.send( {length:word.length, first_letter:word.charAt(0), id:id, nb_life:6});
+});
+
 io.on("connection", (socket) => {
-  id = get_id();
-  word = get_word();
-  tab[id] = word;
-  socket.emit("init", word.split('')[0], id);
-  console.log(get_word().split('')[0]);
-  console.log("Connection !");
+  
 });
 
 server.listen(port, () => {
