239958703f464b2786ef436fe465d6df750a36f3
intla <59796115+ovoleur@users.noreply.github.com>
2022-05-10 12:46:16 +0200

    setting up test

---
+++ server/test/sum.test.ts
@@ -0,0 +1,14 @@
+describe("Sum testing", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(1 + 2).not.toBe(10);
+  });
+  test("adds 1 + 2 to equal 3", () => {
+    expect(1 + 2).not.toBe(11);
+  });
+  test("adds 1 + 2 to equal 3", () => {
+    expect(1 + 2).toBe(3);
+  });
+  test("adds 1 + 2 to equal 3", () => {
+    expect(1 + 2).toBe(3);
+  });
+});
