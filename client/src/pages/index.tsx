5e1c00ddb2ce466a7a50b4028a6caac6359cf6c4
intla <59796115+ovoleur@users.noreply.github.com>
2022-05-11 16:34:33 +0200

    Socket connection client

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,18 +1,18 @@
-import { io } from "socket.io-client";
-
 import { GameModeMenu } from "../components/GameModeMenu";
 import { Layout } from "../components/Layout";
+import { useSocket } from "../utils/hooks";
 
 const Index = () => {
-   const socket = io("ws://localhost:4000");
-   socket.connect();
-   let room = 'room1';
-   socket.emit('create', room);
-   let result = [1,1,1];
-   socket.emit('result', {room : room, result:result});
-    socket.on("roomResult", (arg) => {
-        console.log(arg); // world
-    });
+  // const socket = io("ws://localhost:4000");
+  const socket = useSocket();
+  socket?.connect();
+  let room = "room1";
+  socket?.emit("create", room);
+  let result = [1, 1, 1];
+  socket?.emit("result", { room: room, result: result });
+  socket?.on("roomResult", (arg) => {
+    console.log(arg); // world
+  });
 
   return (
     <Layout>
