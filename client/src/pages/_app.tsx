ec28efb5ca3507e5b7ebc92c287fc7aa6c7a3007
intla <59796115+ovoleur@users.noreply.github.com>
2022-05-12 16:52:16 +0200

    player grid upper case & player context

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -5,14 +5,20 @@ import { AppProps } from "next/app";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { DictionaryContext, SocketContext } from "../utils/context";
+import {
+  DictionaryContext,
+  PlayerContext,
+  SocketContext,
+} from "../utils/context";
 import { io, Socket } from "socket.io-client";
 import { addCreateLobbyEvent, addJoinLobbyEvent } from "src/utils/api";
 import { useRouter } from "next/router";
+import { Player } from "../utils/types";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [dictionary, setDictionnary] = useState<Set<string> | null>(null);
   const [socket, setSocket] = useState<Socket | null>(null);
+  const [player, setPlayer] = useState<Player | null>(null);
   const router = useRouter();
 
   useEffect(() => {
@@ -29,16 +35,18 @@ function MyApp({ Component, pageProps }: AppProps) {
     addJoinLobbyEvent(socket, router);
   }, []);
   return (
-    <SocketContext.Provider value={socket}>
-      <DictionaryContext.Provider
-        value={dictionary !== null ? dictionary : new Set()}
-      >
-        <ChakraProvider resetCSS theme={theme}>
-          <DarkModeSwitch />
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </DictionaryContext.Provider>
-    </SocketContext.Provider>
+    <PlayerContext.Provider value={[player, setPlayer]}>
+      <SocketContext.Provider value={socket}>
+        <DictionaryContext.Provider
+          value={dictionary !== null ? dictionary : new Set()}
+        >
+          <ChakraProvider resetCSS theme={theme}>
+            <DarkModeSwitch />
+            <Component {...pageProps} />
+          </ChakraProvider>
+        </DictionaryContext.Provider>
+      </SocketContext.Provider>
+    </PlayerContext.Provider>
   );
 }
 
--- client/src/utils/context.ts
+++ client/src/utils/context.ts
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import { Socket } from "socket.io-client";
+import { Player } from "./types";
 
 export const DictionaryContext = React.createContext<Set<string>>(new Set());
 export const SocketContext = React.createContext<Socket | null>(null);
+export const PlayerContext = React.createContext<
+  [
+    player: Player | null,
+    setPlayer: Dispatch<SetStateAction<Player | null>> | null
+  ]
+>([null, null]);
