3ff8f373dbdf465433afdf25e07efd3293e930a4
intla <59796115+ovoleur@users.noreply.github.com>
2022-05-13 11:53:51 +0200

    asking usename on joining lobby

--- client/src/pages/lobby/[lobbyId].tsx
+++ client/src/pages/lobby/[lobbyId].tsx
@@ -1,13 +1,33 @@
+import { useDisclosure } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import React from "react";
-import { useSocket } from "src/utils/hooks";
+import { usePlayer, useSocket } from "src/utils/hooks";
+import { CreatePlayerModal } from "../../components/CreatePlayerModal";
 import { Layout } from "../../components/Layout";
+import { PreGameLobby } from "../../components/PreGameLobby";
+import { Lobby } from "../../utils/types";
 
 interface LobbyProps {}
 
-const Lobby: React.FC<LobbyProps> = ({}) => {
+const lobby: Lobby = {
+  id: "1",
+  state: "pre-game",
+  name: "Lobby de bg",
+  totalPlace: 2,
+  currentPlace: 1,
+  playerList: [{ id: "b", name: "bob" }],
+  owner: "b",
+  isPublic: true,
+  mode: "1vs1",
+};
+
+const LobbyPage: React.FC<LobbyProps> = ({}) => {
+  const { state } = lobby;
   const socket = useSocket();
   const router = useRouter();
+  const [player] = usePlayer();
+  const { onClose } = useDisclosure();
+
   let lobbyId = router.query.lobbyId;
   if (lobbyId !== undefined) {
     socket?.emit("join_lobby", {
@@ -16,7 +36,29 @@ const Lobby: React.FC<LobbyProps> = ({}) => {
       playerName: "test",
     });
   }
-  return <Layout>Inside lobby</Layout>;
+
+  if (!player) {
+    return (
+      <Layout>
+        {!player && <CreatePlayerModal isOpen={true} onClose={onClose} />}
+      </Layout>
+    );
+  }
+
+  if (state === "pre-game") {
+    return (
+      <Layout>
+        <PreGameLobby lobby={lobby} />
+      </Layout>
+    );
+  } else if (state === "in-game") {
+    return <Layout variant="large"></Layout>;
+  } else if (state === "finished") {
+    return <Layout></Layout>;
+  } else {
+    // Its supposed to have a state
+    return <Layout>?!</Layout>;
+  }
 };
 
-export default Lobby;
+export default LobbyPage;
