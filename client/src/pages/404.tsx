ec406413cd071f321259929b04abe0b3dd8fe4f2
BenjViaud <benjamin.viaud@etudiant.univ-rennes1.fr>
2022-06-02 16:31:44 +0200

    v1 404 error page

---
+++ client/src/pages/404.tsx
@@ -0,0 +1,32 @@
+import { Flex, Text } from "@chakra-ui/react";
+import { Layout } from "src/components/Layout";
+import { PlayerGrid } from "src/components/player-grid/PlayerGrid";
+
+export default function FourNotFourPage() {
+  return (
+    <Layout variant="large">
+      <Flex direction={"column"}>
+        <Text mb={5} align="center" fontSize={"6xl"}>
+          Erreur 404
+        </Text>
+        <Text mb={5} align="center" fontSize={"3xl"}>
+          La page demandé n'a pas été trouvé !
+        </Text>
+        <PlayerGrid
+          isVisible={true}
+          firstLetter={"U"}
+          wordLength={12}
+          nbLife={1}
+          word={"URL INCONNUE"}
+          triesHistory={[
+            {
+              wordTried: "URL INCONNUE",
+              result: [1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+            },
+          ]}
+          isFinished={false}
+        ></PlayerGrid>
+      </Flex>
+    </Layout>
+  );
+}
