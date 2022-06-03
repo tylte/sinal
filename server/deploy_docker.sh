b62b5b46b2bd53f0f4d33439a98994a3d83e6cf3
intla <59796115+ovoleur@users.noreply.github.com>
2022-06-03 11:32:36 +0200

    update scripts

--- server/deploy_docker.sh
+++ server/deploy_docker.sh
@@ -4,6 +4,8 @@ docker build . -t sinal/server
 
 docker save sinal/server | bzip2 | pv | ssh sinal@51.91.57.172 docker load
 
+docker save sinal/client | bzip2 | pv | ssh sinal@51.91.57.172 docker load
+
 ssh sinal@51.91.57.172 mkdir -p /home/sinal/prod/
 
 scp docker-compose.yml sinal@51.91.57.172:/home/sinal/prod/
