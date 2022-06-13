b89af9451099f73afc88ba9a499b14eb0b37329e
BenjViaud <benjamin.viaud@etudiant.univ-rennes1.fr>
2022-06-13 16:46:49 +0200

    fix s joueurs

--- client/src/components/InGameLobbyBr.tsx
+++ client/src/components/InGameLobbyBr.tsx
@@ -483,7 +483,7 @@ export const InGameLobbyBr: React.FC<InGameLobbyBrProps> = ({
         )}
         {/* The number of player that win */}
         <Text align="center">
-          Joueur qualifiés : {numberPlayerFound}/{numberPlayerWinMax}
+          Joueurs qualifiés : {numberPlayerFound}/{numberPlayerWinMax}
         </Text>
         {/* use when the player is in spectate mode to show the spectate player */}
         {spectate && (
