faf7ba1b04e16af570e4b3286eb0fa43ad50620b
tylte <59796115+ovoleur@users.noreply.github.com>
2023-10-21 10:59:22 +0200

    update + default black theme + no focus dark mode switch

--- client/src/components/DarkModeSwitch.tsx
+++ client/src/components/DarkModeSwitch.tsx
@@ -2,16 +2,26 @@ import { useColorMode, IconButton } from "@chakra-ui/react";
 import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 
 export const DarkModeSwitch = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const isDark = colorMode === "dark";
-  return (
-    <IconButton
-      my={2}
-      mr={4}
-      icon={isDark ? <SunIcon /> : <MoonIcon />}
-      aria-label="Toggle Theme"
-      colorScheme="gray"
-      onClick={toggleColorMode}
-    />
-  );
+    const { colorMode, toggleColorMode } = useColorMode();
+    const isDark = colorMode === "dark";
+    const preventFocus = (e: any) => {
+        if (e.relatedTarget) {
+            // Revert focus back to previous blurring element
+            e.relatedTarget.focus();
+        } else {
+            // No previous focus target, blur instead
+            e.currentTarget.blur();
+        }
+    }
+    return (
+        <IconButton
+            onFocus={preventFocus}
+            my={2}
+            mr={4}
+            icon={isDark ? <SunIcon /> : <MoonIcon />}
+            aria-label="Toggle Theme"
+            colorScheme="gray"
+            onClick={toggleColorMode}
+        />
+    );
 };
